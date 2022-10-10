419b31b5cc6e3f63a5b195735a8deabfeb2b1c56
Sasa <onezerobeatz@gmail.com>
2022-10-10 15:58:51 +0200

    Nullable properties fixed, packages referenced

--- TaskManagement.Api/Models/Task.cs
+++ TaskManagement.Api/Models/Task.cs
@@ -3,10 +3,10 @@
     public class Task
     {
         public int Id { get; set; }
-        public string? Title { get; set; }
-        public string? Description { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
         public DateTime Deadline { get; set; }
         public bool Done { get; set; }
-        public int? TodoListId { get; set; }
+        public int TodoListId { get; set; }
     }
 }
