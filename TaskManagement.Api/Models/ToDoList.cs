79de31743d41649f81fd95d5c8761c8007182bc5
Sasa <onezerobeatz@gmail.com>
2022-10-10 15:49:12 +0200

    initial commit

---
+++ TaskManagement.Api/Models/ToDoList.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManagement.Api.Models
+{
+    public class ToDoList
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public int? UserId { get; set; }
+    }
+}
