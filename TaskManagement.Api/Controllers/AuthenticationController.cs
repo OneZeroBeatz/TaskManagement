861871a284cef8df9054410249cbf25070b4bd54
Sasa <onezerobeatz@gmail.com>
2022-10-12 00:13:56 +0200

    Controller renamed

--- TaskManagement.Api/Controllers/AuthenticationController.cs
+++ TaskManagement.Api/Controllers/AuthenticationController.cs
@@ -1,25 +1,21 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
 using TaskManagement.Application.Messages;
-using TaskManagement.Application.Repositories;
 
 namespace TaskManagement.Api.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class LoginController : ControllerBase
+    public class AuthenticationController : ControllerBase
     {
         public readonly IMediator _mediator;
 
-        public LoginController(IMediator mediator)
+        public AuthenticationController(IMediator mediator)
         {
             _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
         }
-        [HttpPost(Name = "login")]
+
+        [HttpPost]
         public async Task<ActionResult<string>> Login(LoginCommand loginCommand)
         {
             var result = await _mediator.Send(loginCommand);
