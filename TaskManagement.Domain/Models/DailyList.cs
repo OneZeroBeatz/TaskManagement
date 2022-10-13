f97fe44445cd1f43c27d09c08702ad675b8f7bd5
Sasa Momcilovic <sasa.momcilovic@zero21.eu>
2022-10-14 00:49:16 +0200

    Todos fixed

--- TaskManagement.Domain/Models/DailyList.cs
+++ TaskManagement.Domain/Models/DailyList.cs
@@ -5,7 +5,6 @@ public class DailyList
     public int Id { get; set; }
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    //TODO: Consider using DateOnly
     public DateTime Date { get; set; } = DateTime.UtcNow;
     public int UserId { get; set; }
     public User User { get; set; }
