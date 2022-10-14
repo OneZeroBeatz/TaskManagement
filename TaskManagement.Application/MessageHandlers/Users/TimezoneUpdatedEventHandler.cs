3b67e87a9dc7ddc383f71007ae4f695f8e4623dc
Sasa Momcilovic <sasa.momcilovic@zero21.eu>
2022-10-14 17:11:31 +0200

    Decoupling Timezone update business logic from application related job update

---
+++ TaskManagement.Application/MessageHandlers/Users/TimezoneUpdatedEventHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using TaskManagement.Application.Messages.Users;
+using TaskManagement.Infrastructure.Services;
+
+namespace TaskManagement.Application.MessageHandlers.Users
+{
+    public class TimezoneUpdatedEventHandler : IRequestHandler<TimezoneUpdatedEvent>
+    {
+        private readonly INotificationJobUpdateService _notificationJobUpdateService;
+
+        public TimezoneUpdatedEventHandler(INotificationJobUpdateService notificationJobUpdateService)
+        {
+            _notificationJobUpdateService = notificationJobUpdateService ?? throw new ArgumentNullException(nameof(notificationJobUpdateService));
+        }
+
+        public Task<Unit> Handle(TimezoneUpdatedEvent request, CancellationToken cancellationToken)
+        {
+            _notificationJobUpdateService.UpdateJob(request.UserEmail, request.Timezone);
+            return Task.FromResult(Unit.Value);
+        }
+    }
+}
---
+++ TaskManagement.Application/Messages/Users/TimezoneUpdatedEvent.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace TaskManagement.Application.Messages.Users
+{
+    public class TimezoneUpdatedEvent : IRequest
+    {
+        public string UserEmail { get; set; } = string.Empty;
+        public TimeZoneInfo Timezone { get; set; } = TimeZoneInfo.Local;
+    }
+}
